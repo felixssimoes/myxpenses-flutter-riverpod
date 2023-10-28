8257e1b7d7ad6f379c6a11f83a65da1a58ab810d
Felix Simoes <felix.simoes@njiuko.com>
2023-10-28 11:30:21 +0200

    Initial commit

---
+++ lib/core/presentation/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyXpensesApp extends StatelessWidget {
+  const MyXpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'myXpenses',
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'core/presentation/app/app.dart';
+
+void main() {
+  runApp(const MyXpensesApp());
+}
